a7cc4f746e821cca11a85335f0f9f9fd31691cbc
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-12-04 13:02:50 -0300

    feat: busca dinâmica pelo número da versão

--- Assets/Script/Menus/MainMenu.cs
+++ Assets/Script/Menus/MainMenu.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Components")]
+    public TMP_Text StatusLabel;
+
     [Header("Menus")]
     public GameObject SettingsMenu;
+
+    void Awake()
+    {
+        StatusLabel.text = "The Chronicles of Aerinthia\n" +
+            $"Versão: {PlayerSettings.bundleVersion}";
+    }
 
     public void ContinueGame()
     {
