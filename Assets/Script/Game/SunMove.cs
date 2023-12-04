cb2b3eeb42d7e7be1f27bf075b46dfa0acc0b60b
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-12-04 12:54:41 -0300

    feat: implementação do quarto cenário

---
+++ Assets/Script/Game/SunMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMove : MonoBehaviour
+{
+    private GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(GameInfo.PlayerTag);
+    }
+
+    void Update()
+    {
+        Vector2 newPosition = transform.position;
+        newPosition.x = player.transform.position.x - 3;
+        transform.position = newPosition;
+    }
+
+}
