16e0ccec27284a0e0a26f4a6c749347137989cb4
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-12-04 14:24:11 -0300

    fix: atualização de número de versão

--- Assets/Script/Game/GameInfo.cs
+++ Assets/Script/Game/GameInfo.cs
@@ -1,6 +1,6 @@
 ﻿public class GameInfo
 {
-    public static readonly string Version = "0.1.6";
+    public static readonly string Version = "0.1.7";
     public static readonly string PlayerTag = "Player";
 }
 
